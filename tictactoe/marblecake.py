9be1d1f51c836b439307befb85f6cadfe8917464
Alexei <hello@alexei.ro>
2018-03-07 17:21:04 +0200

    Handle user input

--- tictactoe/marblecake.py
+++ tictactoe/marblecake.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import random
+from itertools import cycle
 
-from PyQt4 import QtGui
+from PyQt4 import QtCore, QtGui
 
 
 BUTTON_SIZE = 100
@@ -14,29 +14,53 @@ CHARACTER_0 = '0'
 
 
 class GameWindow(QtGui.QMainWindow):
+    playerSwitch = QtCore.pyqtSignal()
+
     def __init__(self, *args, **kwargs):
         super(GameWindow, self).__init__(*args, **kwargs)
 
         self.setWindowTitle("Tic-tac-toe")
 
         self.statusBar().setSizeGripEnabled(False)
-        self.statusBar().showMessage("Click any square to begin")
 
         board = GameBoard(self)
         self.setCentralWidget(board)
+        board.playerMoves.connect(self.switchPlayers)
 
         width = self.centralWidget().frameGeometry().width()
         height = self.centralWidget().frameGeometry().height() + \
             self.statusBar().frameGeometry().height()
         self.setFixedSize(width, height)
 
+        self.players = [
+            HumanPlayer(CHARACTER_X),
+            HumanPlayer(CHARACTER_0),
+        ]
+        self.players_iterator = cycle(self.players)
+        self.current_player = self.switchPlayers()
+
+    def switchPlayers(self):
+        self.current_player = self.players_iterator.next()
+        self.statusBar().showMessage(
+            "{player} moves".format(player=self.current_player)
+        )
+        return self.current_player
+
+    def getCurrentPlayer(self):
+        return self.current_player
+
 
 class GameBoard(QtGui.QWidget):
+    playerMoves = QtCore.pyqtSignal()
+
     def __init__(self, *args, **kwargs):
         super(GameBoard, self).__init__(*args, **kwargs)
 
+        self.positions = range(9)
+        self.values = [None] * len(self.positions)
+
         self.buttons = []
-        for i in range(9):
+        for i in self.positions:
             self.buttons.append(GameButton(i))
 
         size = 4 * GRID_SPACING + 3 * BUTTON_SIZE + 6 * BUTTON_BORDER_SIZE
@@ -48,9 +72,24 @@ class GameBoard(QtGui.QWidget):
 
         for button in self.buttons:
             grid.addWidget(button, button.row, button.col)
+            button.userInput.connect(self.handleInput)
+
+    def handleInput(self, position):
+        current_player = self.parent().getCurrentPlayer()
+        self.move(position, current_player)
+
+    def move(self, position, player):
+        if self.values[position]:
+            return
+
+        self.values[position] = player
+        self.buttons[position].setText(str(player))
+        self.playerMoves.emit()
 
 
 class GameButton(QtGui.QPushButton):
+    userInput = QtCore.pyqtSignal(int)
+
     def __init__(self, value, *args, **kwargs):
         super(GameButton, self).__init__(*args, **kwargs)
 
@@ -86,4 +125,19 @@ class GameButton(QtGui.QPushButton):
         self.clicked.connect(self.handleClick)
 
     def handleClick(self):
-        self.setText(random.choice([CHARACTER_X, CHARACTER_0]))
+        self.userInput.emit(self.value)
+
+
+class BasePlayer(object):
+    def __init__(self, role):
+        self.role = role
+
+    def __str__(self):
+        return str(self.role)
+
+    def __unicode__(self):
+        return unicode(self.role)
+
+
+class HumanPlayer(BasePlayer):
+    pass
